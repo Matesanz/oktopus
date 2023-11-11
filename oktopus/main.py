f166b980f31da8c840c40461b0931aa1a1ce3eeb
Andrés Matesanz <matesanz.cuadrado@gmail.com>
2023-11-11 14:24:42 +0000

    ✨ allow query as a body request

--- oktopus/main.py
+++ oktopus/main.py
@@ -11,13 +11,11 @@ Then go to http://localhost:8000/docs to see the API documentation.
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from oktopus.data_models import DocNode, Document
+from oktopus.data_models import DocNode, Document, Query
 from oktopus import db
 
 
 app = FastAPI()
-
-app.mount("/", app=StaticFiles(directory="/static", html=True), name="static")
 
 
 # Endpoints
@@ -32,13 +30,13 @@ async def get_documents() -> list[DocNode]:
 
 
 @app.post("/generate", response_model=list[tuple[int, float]])
-async def generate_scores(query: str) -> list[tuple[int, float]]:
+async def generate_scores(query: Query) -> list[tuple[int, float]]:
     """Given a question (query) retrieves the scores for all the documents.
 
     Scores relates to the cosine similarity between the query and the document.
 
     Args:
-        query (str): Query
+        query (Query): Question to be answered by Oktopus based on your data.
 
     Returns:
         List[Tuple[int, float]]: List of (doc_id, score) tuples
@@ -58,3 +56,7 @@ async def get_document_by_id(doc_id: int):
         Document: Document
     """
     return db.documents_db.get(doc_id)
+
+
+# frontend
+app.mount("/", app=StaticFiles(directory="/static", html=True), name="static")
