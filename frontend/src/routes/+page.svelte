e7dcfe2261d9c2a809b31331315201b1f5083cc8
Rubén J <rubenjr@protonmail.com>
2023-11-11 14:39:23 +0200

    💄 Navbar

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -15,13 +15,20 @@
 	}
 </script>
 
-<section class="hero is-fullheight">
+<section class="hero is-fullheight-with-navbar">
+	<div class="hero-head">
+		<nav class="navbar is-primary has-background-primary" aria-label="main navigation">
+			<div class="navbar-brand">
+				<a class="navbar-item" href="https://bulma.io"> OKTOPUS </a>
+			</div>
+		</nav>
+	</div>
 	<div
 		class="hero-body {move_down ? 'is-align-items-end' : ''}"
 		style="transition: 1s ease-in-out;"
 	>
 		<div class="container is-centered has-text-centered">
-			<h1 class="title">Oktoopus</h1>
+			<h1 class="title">Oktopus</h1>
 			<div class="columns is-centered to-bottom">
 				<div class="column is-three-fifths is-justify-content-center">
 					<div class="field has-addons is-justify-content-center">
